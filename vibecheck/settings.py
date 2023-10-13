4b64456bed49d638c7159e8126bf85851a2b6ec1
Affan Khamse <khamseaffan@Affans-Air.lan>
2023-10-13 14:29:04 -0400

    Login UI updated

--- vibecheck/settings.py
+++ vibecheck/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'  # Use database-backed sessions
 SESSION_CACHE_ALIAS = 'default'  # Use the default cache (can be adjusted based on your caching settings)
 
