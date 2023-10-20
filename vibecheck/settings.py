e268d68ff5bc3d139951a09058b3e260272cdadc
adeldelvalle <adel.delvalle@upr.edu>
2023-10-20 18:28:39 -0400

    ,

--- vibecheck/settings.py
+++ vibecheck/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static_src")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "login/static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'  # Use database-backed sessions
