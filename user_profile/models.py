8878f4daf208e13efe5b530c7c1a0a643a551737
yp2353 <yp2353@nyu.edu>
2023-10-31 19:29:43 -0400

    Model updated

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 
-
 # Create your models here.
 class User(models.Model):
-    user_id = models.CharField(max_length=250)
+    user_id = models.CharField(max_length=250, primary_key=True)
     username = models.CharField(max_length=125)
     total_followers = models.IntegerField()
     profile_image_url = models.TextField(null=True)
@@ -13,11 +12,8 @@ class User(models.Model):
     user_city = models.CharField(max_length=255, null=True)
     user_total_friends = models.IntegerField(null=True)
     user_genre = models.CharField(max_length=250, null=True)
-
     def __str__(self) -> str:
         return f"User -> {self.user_id} ->  {self.username}"
-
-
 # storing User vibe
 class Vibe(models.Model):
     user_id = models.CharField(max_length=250)
