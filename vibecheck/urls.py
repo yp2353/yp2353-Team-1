aa59f52884bf35b9e0e94d1f50623ac86b218472
Boyan <bx376@nyu.edu>
2023-11-08 20:37:21 -0500

    Add redirect for root path '/'

--- vibecheck/urls.py
+++ vibecheck/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 # from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
@@ -28,6 +29,7 @@ urlpatterns = [
     path("chatroom/", include("chatroom.urls")),
     path("search/", include("search.urls")),
     path("admin/", admin.site.urls),
+    path("", RedirectView.as_view(url="/login/", permanent=True)),
 ]
 
 if settings.DEBUG:
