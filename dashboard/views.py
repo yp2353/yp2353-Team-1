3bc7c6ccee202dcabf9d1751ea9aa96df9f6767a
yp2353 <yp2353@nyu.edu>
2023-11-13 20:40:52 -0500

    Vibe asynchronous

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,28 +5,16 @@ from datetime import datetime
 from collections import Counter
 import json
 import shutil
-import lyricsgenius
-import openai
-import time
-import numpy as np
-from gradio_client import Client
-import re
+
 import threading
 from user_profile.views import check_and_store_profile
 
 # from dotenv import load_dotenv
-import os
-from utils import get_spotify_token, deduce_audio_vibe
+from utils import get_spotify_token, vibe_calc_threads
 from django.http import JsonResponse
 from user_profile.models import Vibe, UserTop
 from django.utils import timezone
-from dashboard.models import EmotionVector
-
-MAX_RETRIES = 2
-
-client = Client(
-    "https://alfredo273-vibecheck-fasttext.hf.space/--replicas/2gfl7/", serialize=False
-)
+from .vibe_calc import calculate_vibe_async
 
 
 def index(request):
@@ -95,6 +83,11 @@ def index(request):
             {"number": 12, "short_name": "D", "long_name": "December"},
         ]
 
+        vibe_or_not = calculate_vibe(sp, midnight)
+        # Possible values: already_loaded if vibe already calculated within today,
+        # asyn_started if vibe calculation is started and still loading,
+        # no_songs if user has 0 recent songs to analyze
+
         context = {
             "username": username,
             "top_tracks": top_tracks,
@@ -105,6 +98,9 @@ def index(request):
             "iteratorMonth": months,
             "iteratorDay": range(0, 32),
             "currentYear": current_year,
+            "user_id": user_id,
+            "midnight": midnight,
+            "vibe_or_not": vibe_or_not,
         }
 
         extract_tracks(sp)
@@ -187,31 +183,19 @@ def get_recommendations(sp, top_tracks):
     return recommendedtracks
 
 
-def calculate_vibe(request):
-    token_info = get_spotify_token(request)
+def calculate_vibe(sp, midnight):
+    # Check if user vibe already been calculated for today
+    user_info = sp.current_user()
+    user_id = user_info["id"]
+    recent_vibe = Vibe.objects.filter(user_id=user_id, vibe_time__gte=midnight).first()
 
-    if token_info:
-        sp = spotipy.Spotify(auth=token_info["access_token"])
+    if recent_vibe:
+        return "already_loaded"
+    # If vibe today is already in database, RETURN, do not schedule vibe calculation
 
-        # Check if user vibe already been calculated for today
-        user_info = sp.current_user()
-        user_id = user_info["id"]
-        current_time = timezone.now().astimezone(timezone.utc)
-        midnight = current_time.replace(hour=0, minute=0, second=0, microsecond=0)
-        recent_vibe = Vibe.objects.filter(
-            user_id=user_id, vibe_time__gte=midnight
-        ).first()
+    recent_tracks = sp.current_user_recently_played(limit=15)
 
-        if recent_vibe and recent_vibe.user_audio_vibe:
-            vibe_result = recent_vibe.user_audio_vibe
-            if recent_vibe.user_lyrics_vibe:
-                vibe_result += " " + recent_vibe.user_lyrics_vibe
-                description = vibe_description(vibe_result)
-            return JsonResponse({"result": vibe_result, "description": description})
-        # Skips having to perform vibe calculations below
-
-        recent_tracks = sp.current_user_recently_played(limit=15)
-
+    if recent_tracks:
         track_names = []
         track_artists = []
         track_ids = []
@@ -221,48 +205,28 @@ def calculate_vibe(request):
             track_artists.append(track["track"]["artists"][0]["name"])
             track_ids.append(track["track"]["id"])
 
-        # IF TESTING WITH TOP TRACKS INSTEAD OF RECENT
-        """ top_tracks = sp.current_user_top_tracks(limit=10, time_range='short_term')
-        for track in top_tracks['items']:
-            track_names.append(track['name'])
-            track_artists.append(track['artists'][0]['name'])
-            track_ids.append(track['id']) """
+        audio_features_list = sp.audio_features(track_ids)
 
-        if track_ids:
-            audio_features_list = sp.audio_features(track_ids)
-            audio_vibe, lyric_vibe = check_vibe(
-                track_names, track_artists, track_ids, audio_features_list
-            )
-            vibe_result = audio_vibe
-            if lyric_vibe:
-                vibe_result += " " + lyric_vibe
-
-            current_time = timezone.now().astimezone(timezone.utc)
-            description = vibe_description(vibe_result)
-            vibe_data = Vibe(
-                user_id=user_id,
-                vibe_time=current_time,
-                user_lyrics_vibe=lyric_vibe,
-                user_audio_vibe=audio_vibe,
-                recent_track=track_ids,
-                user_acousticness=get_feature_average(
-                    audio_features_list, "acousticness"
+        # Schedule asynchronous vibe calculation
+        # But first check if a thread is already running and calculating!
+        if user_id not in vibe_calc_threads:
+            vibe_thread = threading.Thread(
+                target=calculate_vibe_async,
+                args=(
+                    track_names,
+                    track_artists,
+                    track_ids,
+                    audio_features_list,
+                    user_id,
                 ),
-                user_danceability=get_feature_average(
-                    audio_features_list, "danceability"
-                ),
-                user_energy=get_feature_average(audio_features_list, "energy"),
-                user_valence=get_feature_average(audio_features_list, "valence"),
             )
-            vibe_data.save()
-        else:
-            vibe_result = "Null"
+            vibe_thread.start()
+            vibe_calc_threads[user_id] = vibe_thread
 
-        return JsonResponse({"result": vibe_result, "description": description})
+        return "asyn_started"
+
     else:
-        # No token, redirect to login again
-        # ERROR MESSAGE HERE?
-        return redirect("login:index")
+        return "no_songs"
 
 
 def logout(request):
@@ -340,98 +304,6 @@ def extract_tracks(sp):
     shutil.move("day_data.json", "login/static/login/day_data.json")
 
 
-def check_vibe(track_names, track_artists, track_ids, audio_features_list):
-    audio_final_vibe = deduce_audio_vibe(audio_features_list)
-
-    lyrics_vibes = deduce_lyrics(track_names, track_artists, track_ids)
-    lyrics_final_vibe = lyrics_vectorize(lyrics_vibes)
-
-    if not lyrics_final_vibe:
-        lyrics_final_vibe = None
-
-    return audio_final_vibe, lyrics_final_vibe
-
-
-def deduce_lyrics(track_names, track_artists, track_ids):
-    genius = lyricsgenius.Genius(os.getenv("GENIUS_CLIENT_ACCESS_TOKEN"))
-
-    lyrics_vibes = []
-
-    # CHECK TRACK DATABASE BASED ON ID, ADD TRACK VIBE TO LYRICS_VIBES IF ALREADY IN DATABASE!!!
-
-    lyrics_data = {}
-    for track, artist, id in zip(track_names, track_artists, track_ids):
-        # SKIP TRACKS ALRDY IN DATABASE!!!
-        query = f'"{track}" "{artist}"'
-        song = genius.search_song(query)
-        if song:
-            # Genius song object sometimes has trailing space, so need to strip
-            geniusTitle = song.title.lower().replace("\u200b", " ").strip()
-            geniusArtist = song.artist.lower().replace("\u200b", " ").strip()
-            if geniusTitle == track.lower() and geniusArtist == artist.lower():
-                print("Inputting lyrics..")
-                lyrics_data[(track, artist, id)] = song.lyrics
-
-    openai.api_key = os.getenv("OPEN_AI_TOKEN")
-
-    for (track, artist, id), lyrics in lyrics_data.items():
-        short_lyrics = lyrics[:2048]
-        retries = 0
-        while retries < MAX_RETRIES:
-            try:
-                print(f"Processing song. Track: {track}, Artist: {artist}, ID: {id}")
-                print(f"Lyrics: {short_lyrics[:200]}")
-                response = openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo",
-                    messages=[
-                        {"role": "system", "content": "You are a helpful assistant."},
-                        {
-                            "role": "user",
-                            "content": f"You are a mood analyzer that can only return a single word. Based on these song lyrics, return a single word that matches this song's mood: '{short_lyrics}'",
-                        },
-                    ],
-                    request_timeout=5,
-                )
-                vibe = response.choices[0].message["content"].strip()
-                checkLength = vibe.split()
-                if len(checkLength) == 1:
-                    lyrics_vibes.append(vibe.lower())
-
-                    # INSERT VIBE HERE INTO TRACK DATABASE!!!!!
-                print(f"The vibe for {track} is: {vibe}")
-
-                break
-            except Exception as e:
-                print(f"Error processing the vibe for {track}: {e}")
-                retries += 1
-
-            if retries >= MAX_RETRIES:
-                print(f"Retries maxed out processing the vibe for {track}.")
-                break
-            else:
-                time.sleep(1)
-
-    return lyrics_vibes
-
-
-def lyrics_vectorize(lyrics_vibes):
-    if lyrics_vibes:
-        for i in lyrics_vibes:
-            print("Lyrics vibes: " + i)
-        avg_lyr_vibe = average_vector(lyrics_vibes)
-        closest_emotion = find_closest_emotion(avg_lyr_vibe)
-        return str(closest_emotion)
-    else:
-        return None
-
-
-def string_to_vector(str):
-    clean = re.sub(r"[\[\]\n\t]", "", str)
-    clean = clean.split()
-    clean = [float(e) for e in clean]
-    return clean
-
-
 """
 # On huggingface spaces
 def get_vector(word, model):
@@ -443,119 +315,20 @@ def get_vector(word, model):
 """
 
 
-def average_vector(words):
-    # Compute the average vector for a list of words.
-    vectors = []
-    for word in words:
-        str_vector = client.predict("get_vector", word, api_name="/predict")
-        vector = string_to_vector(str_vector)
-        vectors.append(vector)
+def get_task_status(request, user_id, midnight):
+    # Check if there is a result in the database
+    recent_vibe = Vibe.objects.filter(user_id=user_id, vibe_time__gte=midnight).first()
 
-    return np.mean(vectors, axis=0)
-
-
-def find_closest_emotion(final_vibe):
-    emotion_words = [
-        "happy",
-        "sad",
-        "angry",
-        "anxious",
-        "content",
-        "excited",
-        "bored",
-        "nostalgic",
-        "frustrated",
-        "hopeful",
-        "afraid",
-        "confident",
-        "jealous",
-        "grateful",
-        "lonely",
-        "rebellious",
-        "relaxed",
-        "amused",
-        "curious",
-        "ashamed",
-        "sympathetic",
-        "disappointed",
-        "proud",
-        "enthusiastic",
-        "empathetic",
-        "shocked",
-        "calm",
-        "inspired",
-        "indifferent",
-        "romantic",
-        "tense",
-        "euphoric",
-        "restless",
-        "serene",
-        "sensual",
-        "reflective",
-        "playful",
-        "dark",
-        "optimistic",
-        "mysterious",
-        "seductive",
-        "regretful",
-        "detached",
-        "melancholic",
-    ]
-
-    max_similarity = -1
-    closest_emotion = None
-    for word in emotion_words:
-        word_vec = get_emotion_vector(word)
-        similarity = cosine_similarity(final_vibe, word_vec)
-        if similarity > max_similarity:
-            max_similarity = similarity
-            closest_emotion = word
-    return closest_emotion
-
-
-def cosine_similarity(vec_a, vec_b):
-    return np.dot(vec_a, vec_b) / (np.linalg.norm(vec_a) * np.linalg.norm(vec_b))
-
-
-def get_feature_average(list, feature):
-    total = sum(track[feature] for track in list)
-    average = total / len(list)
-    return average
-
-
-def get_emotion_vector(input_emotion):
-    input_emotion = input_emotion.lower()
-    vector_str = EmotionVector.objects.filter(emotion=input_emotion).first()
-
-    if not vector_str:
-        # We should always get vector string stored in our database,
-        # but if somehow is not in database..
-        vector_str = client.predict("get_vector", input_emotion, api_name="/predict")
+    if recent_vibe and recent_vibe.user_audio_vibe:
+        vibe_result = recent_vibe.user_audio_vibe
+        if recent_vibe.user_lyrics_vibe:
+            vibe_result += " " + recent_vibe.user_lyrics_vibe
+        description = recent_vibe.description
+        response_data = {
+            "status": "SUCCESS",
+            "result": vibe_result,
+            "description": description,
+        }
+        return JsonResponse(response_data)
     else:
-        vector_str = vector_str.vector
-
-    return string_to_vector(vector_str)
-
-
-def vibe_description(final_vibe):
-    print(final_vibe)
-    openai.api_key = os.getenv("OPEN_AI_TOKEN")
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {
-                "role": "user",
-                "content": f"This is the output of a program that takes listening history of a person (spotify "
-                f"features) and their lyrics and classifies a daily final vibe. Take the daily final vibe, "
-                f"this being: '{final_vibe}', and briefly describe today's person music vibe and energy as "
-                f"if you were talking to them. Use pop culture terms, artists as references as be brief "
-                f"but precise.",
-            },
-        ],
-        request_timeout=50,
-    )
-
-    response = response.choices[0].message["content"].strip()
-
-    return response
+        return JsonResponse({"status": "PENDING"})
