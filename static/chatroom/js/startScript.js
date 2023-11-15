cf453f24044e78c32415127da05a2d4954f6a5dd
Joshua Alfred Jayapal <joshua.jayapal@gmail.com>
2023-11-15 18:01:11 -0500

    js commit for chat

--- static/chatroom/js/startScript.js
+++ static/chatroom/js/startScript.js
@@ -1,18 +1,65 @@
 // startScript.js
+var sender = undefined
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
+function pollForMessages(roomID) {
+    setInterval(function() {
+        $.ajax({
+            type: 'GET',
+            url: `/chatroom/api/latest_messages/${roomID}/`,
+            success: function(response) {
+                if (response.hasOwnProperty("messages")) {
+                    const messages = response.messages || [];
+                    for (const message of messages) {
+                        handleMessage(message, false);
+                    }
+                }
+            },
+            error: function(error) {
+                console.error('Error fetching latest messages:', error);
+            }
+        });
+    }, 500);  // Adjust the interval (in milliseconds) based on your needs
+}
 
 // Function to join a room
 function joinRoom(roomID) {
     $.ajax({
         type: 'POST',
-        url: 'chatroom/api/chatroom/',
-        data: { roomID: roomID },
+        url: '/chatroom/api/chatroom/',
+        data: { 
+            csrfmiddlewaretoken: getCookie('csrftoken'),
+            type: "join_room",
+            roomID: roomID 
+        },
         success: function (response) {
-            // Handle the response (e.g., update chat messages)
-            console.log('Joined room:', roomID);
-            const messages = response.messages || [];
-            for (const message of messages) {
-                handleMessage(message);
+            // console.log('Joined room:', roomID);
+            // console.log(response)
+            if (response.hasOwnProperty("messages")) {
+                const messages = response.messages || [];
+                for (const message of messages) {
+                    handleMessage(message);
+                }
             }
+
+            // Update the sender value
+            sender = response.sender || 'Anonymous';
+            console.log("User is ", sender);
+
+            pollForMessages(roomID);
         },
         error: function (error) {
             console.error('Error joining room:', roomID, error);
@@ -21,22 +68,30 @@ function joinRoom(roomID) {
 }
 
 // Function to handle incoming chat messages
-function handleMessage(data) {
-    console.log(data.type);
-    console.log(data.message);
-
+function handleMessage(data ) {
+    // console.log(data.type);
+    // console.log(data.message);
+  
+    
     if (data.type === 'chat_message') {
         // Handle chat messages
-        const sender = data.sender || 'Anonymous';  // Default to 'Anonymous' if sender is not provided
+        
+        sender = data.sender || 'Anonymous';  
         const message = data.message;
-
+        
         // Append the message to the chat interface
         $('#chat-messages').append(
             '<div class="message incoming">' +
             '<strong>' + sender + ':</strong> ' + message + '</div>'
         );
+
+        // Scroll to the bottom of the chat-messages container
+        const chatMessagesContainer = $('#chat-messages');
+        chatMessagesContainer.scrollTop(chatMessagesContainer.prop("scrollHeight"));
     }
+    
 }
+
 
 // Attach event listener to room list items
 $('#room-list').on('click', 'li', function (e) {
@@ -47,4 +102,6 @@ $('#room-list').on('click', 'li', function (e) {
     $('#room-container').css('visibility', 'visible');
     $('#room-name').html($(this).html());
 });
+
+
 
