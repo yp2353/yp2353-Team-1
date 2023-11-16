01896ec84e5dadd2cb8c98f09344aa52556efecc
khamseaffan <khamseaffan@gmail.com>
2023-11-15 20:18:19 -0500

    settings.py updated

--- vibecheck/settings.py
+++ vibecheck/settings.py
@@ -90,7 +90,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [("redis-server.ki72ah.ng.0001.use1.cache.amazonaws.com", 6379)],
         },
     },
 }
