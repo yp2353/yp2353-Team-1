c5a24d9e8f3f3c15ea95f8d3f5acc499a1dc4901
khamseaffan <khamseaffan@gmail.com>
2023-11-19 21:29:35 -0500

    redis server link added

--- vibecheck/settings.py
+++ vibecheck/settings.py
@@ -90,8 +90,8 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            # "hosts": [("aws-my-1klkhg28tlkoc.ki72ah.0001.use1.cache.amazonaws.com", 6379)],
-            "hosts": [("127.0.0.1", 6379)],
+            "hosts": [("redis-server.ki72ah.ng.0001.use1.cache.amazonaws.com", 6379)],
+            # "hosts": [("127.0.0.1", 6379)],
         },
     },
 
