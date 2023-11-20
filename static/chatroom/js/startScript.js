c62b595496e3ef3147d8512f4c3c6468937722e0
khamseaffan <khamseaffan@gmail.com>
2023-11-19 22:34:47 -0500

    new URL

--- static/chatroom/js/startScript.js
+++ static/chatroom/js/startScript.js
@@ -1,7 +1,5 @@
 const roomName = 'global';
-const chatSocket = new WebSocket(
-'ws://' + window.location.host + '/ws/chatroom/'
-);
+const chatSocket = new WebSocket('ws://redis-server.ki72ah.ng.0001.use1.cache.amazonaws.com/ws/chatroom/');
 
 // const chatSocket = new WebSocket('ws://team-dev.eba-gbtqzcqx.us-east-1.elasticbeanstalk.com/ws/chatroom/');
 
