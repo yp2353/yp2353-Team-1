64057696f3e2d8a569a1a7922aeaefc4b7d5baa0
khamseaffan <khamseaffan@gmail.com>
2023-11-21 16:36:53 -0500

    asgi new update

--- vibecheck/asgi.py
+++ vibecheck/asgi.py
@@ -12,18 +12,18 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from dotenv import load_dotenv
-from chatroom import routing  # noqa isort:skip
 import django
 
-
+django.setup()
 load_dotenv()
 
 # If DJANGO_SETTINGS_MODULE is unset, default to the local settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vibecheck.settings")
-django.setup()
+
 # Import websocket application here, so apps from django_application are loaded first
 # This application object is used by any ASGI server configured to use this file.
 
+from chatroom import routing  # noqa isort:skip
 application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
