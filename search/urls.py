abcddd0a7a5b82d39ec5d4811d7658a1f26cda92
yp2353 <yp2353@nyu.edu>
2023-11-21 16:21:27 -0500

    Implemented view_profile

--- search/urls.py
+++ search/urls.py
@@ -6,9 +6,4 @@ app_name = "search"
 urlpatterns = [
     path("", views.open_search_page, name="search_page"),
     path("search_user/", views.user_search, name="search_user"),
-    path(
-        "process_friend_request/<str:friend_user_id>",
-        views.process_friend_request,
-        name="process_friend_request",
-    ),
 ]
--- view_profile/urls.py
+++ view_profile/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = "view_profile"
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("other/<str:other_user_id>/", views.other, name="other"),
+    path("process_fr/", views.process_fr, name="process_fr"),
 ]
