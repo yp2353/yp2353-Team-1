578177ac714306950e64f0ece1ac527a2b26d18e
khamseaffan <khamseaffan@gmail.com>
2023-11-23 18:59:20 -0500

    setup() remodev and setting chnaged and config up

--- vibecheck/settings.py
+++ vibecheck/settings.py
@@ -93,7 +93,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("redis-cluster.ki72ah.ng.0001.use1.cache.amazonaws.com:6379", 6379)],
+            "hosts": [("redis-cluster.ki72ah.ng.0001.use1.cache.amazonaws.com", 6379)],
             # "hosts": [("127.0.0.1", 6379)],
         },
     },
