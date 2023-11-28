e9b8fd06fb1e9a5a58e321b98c49e3ba6dab9ac8
yp2353 <yp2353@nyu.edu>
2023-11-27 23:20:01 -0500

    Add current track

--- user_profile/views.py
+++ user_profile/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render, redirect
 from dotenv import load_dotenv
 from .models import User
 from django.contrib import messages
+from .forms import SearchForm
+from utils import get_spotify_token
+import spotipy
 
 # Load variables from .env
 load_dotenv()
@@ -15,11 +18,25 @@ def check_and_store_profile(request):
     # Check if the user is authenticated
     if request.user.is_authenticated:
         user = request.user
+        form = SearchForm()
+
+        token_info = get_spotify_token(request)
+        if token_info:
+            sp = spotipy.Spotify(auth=token_info["access_token"])
+        else:
+            sp = None
+
+        if user.track_id is not None and sp is not None:
+            track = sp.track(user.track_id)
+        else:
+            track = None
 
         context = {
             "username": user.username,
             "user": user,
             "default_image_path": "user_profile/blank_user_profile_image.jpeg",
+            "SearchForm": form,
+            "track": track,
         }
         return render(request, "user_profile/user_profile.html", context)
     else:
@@ -85,3 +102,89 @@ def update(request):
 
     messages.error(request, "Edit profile failed, please try again later.")
     return redirect("login:index")
+
+
+def search(request):
+    # Check if the user is authenticated
+    if request.user.is_authenticated:
+        user = request.user
+        token_info = get_spotify_token(request)
+        if token_info:
+            sp = spotipy.Spotify(auth=token_info["access_token"])
+
+            if request.method == "GET":
+                form = SearchForm(request.GET)
+
+                if form.is_valid():
+                    search_query = form.cleaned_data['search_query']
+                    results = []
+                    track_results = sp.search(q=search_query, type='track', limit=10)
+                    for track in track_results['tracks']['items']:
+                        track_info = {
+                            'id': track['id'],
+                            'name': track['name'],
+                            'artists': ', '.join([artist['name'] for artist in track['artists']]),
+                            'album': track['album']['name'],
+                            'image': track['album']['images'][0]['url'],
+                            'release_date': track['album']['release_date'],
+                        }
+                        results.append(track_info)
+                    
+                else:
+                    results = None
+            
+            else:
+                form = SearchForm()
+                results = None
+
+            if user.track_id is not None and sp is not None:
+                track = sp.track(user.track_id)
+            else:
+                track = None
+
+            context = {
+                "username": user.username,
+                "user": user,
+                "default_image_path": "user_profile/blank_user_profile_image.jpeg",
+                "SearchForm": form,
+                "results": results,
+                "track": track,
+            }
+            return render(request, "user_profile/user_profile.html", context)
+        
+    messages.error(
+        request,
+        "Profile track search failed, please try again later.",
+    )
+    return redirect("login:index")
+
+
+def changeTrack(request):
+    if request.method == "POST":
+        user_id = request.POST.get("user_id", None)
+        action = request.POST.get("action", None)
+        
+        if user_id is not None and action is not None:
+            user = User.objects.filter(user_id=user_id).first()
+            if not user:
+                # User doesnt exist in database?
+                messages.error(
+                    request,
+                    "Changing track failed, please try again later. User unavailable.",
+                )
+                return redirect("login:index")
+
+            if action == "remove":
+                user.track_id = None
+                user.save()
+            
+            elif action == "add":
+                track_id = request.POST.get("track_id", None)
+                if track_id:
+                    user.track_id = track_id
+                    user.save()
+            
+            return redirect("user_profile:profile_page")
+        
+    messages.error(request, "Change track failed, please try again later.")
+    return redirect("dashboard:index")
