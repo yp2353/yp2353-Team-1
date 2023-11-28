7cc4a9e7e99a17b61b970f2d15f32836b337a996
Joshua Alfred Jayapal <joshua.jayapal@gmail.com>
2023-11-27 20:36:24 -0500

    ✅ geolocation and storing in DB

--- chatroom/urls.py
+++ chatroom/urls.py
@@ -7,6 +7,4 @@ app_name = "chatroom"
 
 urlpatterns = [
     path("", views.open_chatroom, name="open_chatroom"),
-    path("api/chatroom/", views.chatroom_api, name="chatroom_api"),
-    path('api/latest_messages/<str:room_id>/', views.get_latest_messages, name='get_latest_messages'),   
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- vibematch/models.py
+++ vibematch/models.py
@@ -1,3 +1,12 @@
-# from django.db import models
+from django.db import models
+from django.conf import settings
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserLocation(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,  # Use the custom user model
+        on_delete=models.CASCADE
+    )
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    created_at = models.DateTimeField(auto_now_add=True)
