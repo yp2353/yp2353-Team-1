5f010a5c9204378ed007120fd6b17e3fbaefb288
Joshua Alfred Jayapal <joshua.jayapal@gmail.com>
2023-11-27 19:19:31 -0500

    deleting pusher API chatroom

--- chats/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class ChatMessage(models.Model):
-    content = models.TextField()
-    sender = models.CharField(max_length=255)
-    timestamp = models.DateTimeField(auto_now_add=True)
